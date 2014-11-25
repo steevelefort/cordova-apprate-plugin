ea36103f644e1c339a566e9e28542f30988026db
Steeve Lefort <steeve@lefort-software.com>
2014-11-25 13:52:47 +0100

    Test

--- README.md
+++ README.md
@@ -1,20 +1,49 @@
 #appRatePlugin
 =======================
 
-Open store review window / Cordova3
+Open store rate window for Apache Cordova 3.x
 Works on iOS and Android
-
 
 ##INSTALLATION
 
 In your Cordova project directory : 
-cordova plugin add https://github.com/steevelefort/cordova-links-plugin
-
-Verify if you config.xml file contains :
-
-&lt;feature name="LinksPlugin"&gt;
-	    &lt;param name="ios-package" value="LinksPlugin" /&gt;
-&lt;/feature&gt;
+cordova plugin add https://github.com/steevelefort/cordova-apprate-plugin
 
 ##USE IT
-linksPlugin.openlink("http://www.google.fr");
+
+	appRatePlugin.start({ 
+	    AppStoreId: "012345678",
+	    PlayStoreId : "com.domain.app",
+	    PopupTitle : 'Rate my app',
+	    PopupText : 'Do you like this app ? Please, rate it !',
+	    ButtonRate : "Rate this app",
+	    ButtonLater : "Remind me later",
+	    ButtonStop : "No, thanks",
+	    AppVersion : "1"
+	});
+
+##OPTIONS
+
+###AppStoreId
+Your app ID in Apple App Store
+
+###PlayStoreId
+Your app ID in Google Play
+
+###PopupTitle
+Title of the popup
+
+###PopupText
+Message display in the popup
+
+###ButtonRate
+Your label for the "rate now" button
+
+###ButtonLater
+Your label for the "remind me later" button
+
+###ButtonStop
+Your label for the "No, thanks" button
+
+###AppVersion
+You app version. Don't forget to change it for each version.
--- www/AppRatePlugin.js
+++ www/AppRatePlugin.js
@@ -6,8 +6,43 @@ function AppRatePlugin() {}
 
 AppRatePlugin.prototype.start = function(options) {
 
-    if (device.platform=='iOS') linksPlugin.openlink("http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id="+options.AppStoreId+"&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8");
-    if (device.platform=='Android') linksPlugin.openlink("http://play.google.com/store/apps/details?id="+options.PlayStoreId);
+    if (typeof options.AppVersion == "undefined") { options.AppVersion = "1"; }   
+    if (typeof options.PopupTitle == "undefined") { options.PopupTitle = "Rate this app"; }   
+    if (typeof options.PopupText == "undefined") { options.PopupText = "If you enjoy using this app, would you mind taking a moment to rate it?"; }   
+    if (typeof options.ButtonRate == "undefined") { options.ButtonRate = "Yes, rate it"; }   
+    if (typeof options.ButtonLater == "undefined") { options.ButtonLater = "Remind me later"; }   
+    if (typeof options.ButtonStop == "undefined") { options.ButtonStop = "No, thanks"; }   
+
+    if (localStorage.getItem("count"+options.AppVersion) === null){
+        localStorage.setItem("count"+options.AppVersion,1);
+    } else {
+        if (parseInt(localStorage.getItem("count"+options.AppVersion))>0)
+            localStorage.setItem("count"+options.AppVersion,parseInt(localStorage.getItem("count"+options.AppVersion))+1);       
+    }
+
+    if (parseInt(localStorage.getItem("count"+options.AppVersion))>2){
+        navigator.notification.confirm(
+            options.PopupText,
+            function(index){ 
+                switch(index) {
+                    case 1:
+                        if (device.platform=='iOS') linksPlugin.openlink("http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id="+options.AppStoreId+"&pageNumber=0&sortOrdering=2&type=Purple+Software&mt=8");
+                        if (device.platform=='Android') linksPlugin.openlink("http://play.google.com/store/apps/details?id="+options.PlayStoreId);
+                        localStorage.setItem("count"+options.AppVersion,0);
+                        break;
+                    case 2:
+                        localStorage.removeItem("count"+options.AppVersion);
+                        break;
+                    case 3:
+                        localStorage.setItem("count"+options.AppVersion,0);
+                        break;
+                }
+            },
+            options.PopupTitle,       
+            [options.ButtonRate, options.ButtonLater, options.ButtonStop]
+        );
+    }
+
 }
 
 var appRatePlugin = new AppRatePlugin();
